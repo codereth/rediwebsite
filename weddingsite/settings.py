96126ac847bd55c690fbbac7f13e44e6211ecbac
codereth <blazebigboi@gmail.com>
2023-05-03 15:31:34 +0300

    Added gunicorn

--- weddingsite/settings.py
+++ weddingsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jtytlef!(=a5klv*)a+mx4ol)zr^x9krw*oijx!%@1bw2b78et
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
